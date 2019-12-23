8571ddea1943d4ea0d02758ea3ad17aa7cfe0ec1
zhenquan.qiu <gnsyxiang@163.com>
2019-12-23 10:27:10 +0800

    feat: add get version script

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
-TOP_DIR=./
-RES_DIR=$TOP_DIR/res
-SCRIPT_DIR=$RES_DIR/script
+TOP_DIR=.
+BUILD_AUX_DIR=${TOP_DIR}/build-aux
+
+# generate version number
+generate_version_num()
+{
+    cd ${BUILD_AUX_DIR}
+    ./generate-version.sh
+    cd -  >/dev/null 2>&1
+}
 
 # use autotools to configure
 autotools_configure()
 {
-  autoreconf -fvi -Im4
+    autoreconf -fvi -Im4
 }
+
+generate_version_num
 
 autotools_configure
