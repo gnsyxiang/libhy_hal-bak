a3bf1589da59a880084905b69b59256f9eb4b7b3
zhenquan.qiu <gnsyxiang@163.com>
2020-03-29 15:07:58 +0800

    build: add rtt compile build for config

--- build.sh
+++ build.sh
@@ -10,10 +10,23 @@ echo -en "\nplease select platform: "
 read  num
 if [[ $num =~ ^-?[0-9]+$ ]]; then
     if [[ $num = 1 ]]; then
+        echo -en "\nwhether to support tinyalsa[yes/no, default no]: "
+        read support_tinyalsa
+        if [[ ${support_tinyalsa} = yes ]]; then
+            echo -en "\ntinyalsa install path: "
+            read tinyalsa_install_path
+            config_str="${config_str} --enable-audio --with-libtinyalsa=${tinyalsa_install_path}"
+        fi
+
         config_str="${config_str} --enable-linux"
         program_prefix=linux-
     elif [[ $num = 2 ]]; then
-        config_str="${config_str} --enable-rtt"
+        echo -en "\nthe toolchains path[/dirs/bin/arm-none-eabi-]: "
+        read toolchains_dir
+        GCC=${toolchains_dir}gcc
+        CXX=${toolchains_dir}g++
+
+        config_str="${config_str} CC=${GCC} --enable-rtt"
         program_prefix=rtt-
     fi
 else
@@ -21,16 +34,6 @@ else
     exit -1
 fi
 
-echo -en "\nwhether to support tinyalsa[yes/no, default no]: "
-read support_tinyalsa
-if [[ ${support_tinyalsa} = yes ]]; then
-    echo -en "\ntinyalsa install path: "
-    read tinyalsa_install_path
-    config_str="${config_str} --enable-audio --with-libtinyalsa=${tinyalsa_install_path}"
-fi
-
-GCC=${TOOLCHAINS_DIR}gcc
-CXX=${TOOLCHAINS_DIR}g++
 
 echo "---------------------------------------------"
 echo "detect system, initialize basic configuration"
@@ -41,7 +44,6 @@ echo "-----------------------------------"
 echo "run configure for generate Makefile"
 echo "-----------------------------------"
 ./configure \
-    CC=${GCC} CXX=${CXX} \
     ${config_str} \
     --prefix=${install_path} \
     --program-prefix=${program_prefix}
