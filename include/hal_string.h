e8fc49f60074e21b391e25f3c3eb18cb1fc4589c
zhenquan.qiu <gnsyxiang@163.com>
2020-04-05 15:55:41 +0800

    feat: add Hal_strstr and Hal_memcpy for string

--- include/hal_string.h
+++ include/hal_string.h
@@ -24,11 +24,13 @@
 extern "C" {
 #endif
 
-#include "hal_config.h"
+#include "hal_type.h"
 
 hal_char_t *Hal_strcpy(hal_char_t *dest, const hal_char_t *src);
 hal_char_t *Hal_strncpy(hal_char_t *dest, const hal_char_t *src, size_t n);
 size_t Hal_strlen(const hal_char_t *s);
+hal_char_t *Hal_strstr(const hal_char_t *haystack, const char *needle);
+void *Hal_memcpy(void *dest, const void *src, size_t n);
 
 #ifdef __cplusplus
 }
