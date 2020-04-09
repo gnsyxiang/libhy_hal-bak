83fa95a7304de7d92f6a1e83cdfbe3ef13d27cb6
zhenquan.qiu <qiuzhenquan@unisound.com>
2020-04-09 18:27:24 +0800

    feat: add linux mutex interface

---
+++ include/hal_mutex.h
@@ -0,0 +1,44 @@
+/**
+ *
+ * Release under GPL-3.0.
+ * 
+ * @file    hal_mutex.h
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    09/04 2020 17:43
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        09/04 2020      create the file
+ * 
+ *     last modified: 09/04 2020 17:43
+ */
+#ifndef __LIBHAL_INCLUDE_HAL_MUTEX_H_
+#define __LIBHAL_INCLUDE_HAL_MUTEX_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "hal_type.h"
+
+typedef void * ThreadMutexHandle_t;
+
+ThreadMutexHandle_t HalMutexInit(void);
+hal_int32_t HalMutexDestroy(ThreadMutexHandle_t handle);
+
+hal_int32_t HalMutexLock(ThreadMutexHandle_t handle);
+hal_int32_t HalMutexTryLock(ThreadMutexHandle_t handle);
+
+hal_int32_t HalMutexUnLock(ThreadMutexHandle_t handle);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __LIBHAL_INCLUDE_HAL_MUTEX_H_
+
---
+++ src/hal_mutex.c
@@ -0,0 +1,113 @@
+/**
+ * 
+ * Release under GPL-3.0.
+ * 
+ * @file    hal_mutex.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    09/04 2020 17:42
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        09/04 2020      create the file
+ * 
+ *     last modified: 09/04 2020 17:42
+ */
+#include "hal_mutex.h"
+
+#ifdef HAVE_LINUX_HAL
+#include <pthread.h>
+
+typedef struct {
+    pthread_mutex_t mutex;
+} hal_mutex_context_t;
+#define HAL_MUTEX_CONTEXT_LEN (sizeof(hal_mutex_context_t))
+
+static inline hal_mutex_context_t *_context_init(void)
+{
+    hal_mutex_context_t *context = Hal_calloc(1, HAL_MUTEX_CONTEXT_LEN);
+    if (NULL == context) {
+        HalLogE("hal calloc faild \n");
+        return NULL;
+    }
+    return context;
+}
+
+static inline void _context_final(hal_mutex_context_t **context)
+{
+    if (NULL != *context) {
+        Hal_free(*context);
+        *context = NULL;
+    }
+}
+
+ThreadMutexHandle_t HalMutexInit(void)
+{
+    hal_mutex_context_t *context = _context_init();
+    if (NULL == context) {
+        HalLogE("context init faild \n");
+        return NULL;
+    }
+
+    pthread_mutex_init(&context->mutex, NULL);
+
+    return context;
+}
+
+hal_int32_t HalMutexDestroy(ThreadMutexHandle_t handle)
+{
+    if (NULL == handle) {
+        HalLogE("the handle is NULL \n");
+        return -1;
+    }
+    hal_mutex_context_t *context = handle;
+
+    pthread_mutex_destroy(&context->mutex);
+
+    return 0;
+}
+
+hal_int32_t HalMutexLock(ThreadMutexHandle_t handle)
+{
+    if (NULL == handle) {
+        HalLogE("the handle is NULL \n");
+        return -1;
+    }
+    hal_mutex_context_t *context = handle;
+
+    pthread_mutex_lock(&context->mutex);
+
+    return 0;
+}
+
+hal_int32_t HalMutexTryLock(ThreadMutexHandle_t handle)
+{
+    if (NULL == handle) {
+        HalLogE("the handle is NULL \n");
+        return -1;
+    }
+    hal_mutex_context_t *context = handle;
+
+    pthread_mutex_trylock(&context->mutex);
+
+    return 0;
+}
+
+hal_int32_t HalMutexUnLock(ThreadMutexHandle_t handle)
+{
+    if (NULL == handle) {
+        HalLogE("the handle is NULL \n");
+        return -1;
+    }
+    hal_mutex_context_t *context = handle;
+
+    pthread_mutex_unlock(&context->mutex);
+
+    return 0;
+}
+#endif
+
