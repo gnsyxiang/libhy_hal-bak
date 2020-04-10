5cdc7697704851b7ad83102f24948b152e52311c
zhenquan.qiu <qiuzhenquan@unisound.com>
2020-04-10 13:53:49 +0800

    test: add test for sem and mutex

--- example/producer_comsumer.c
+++ example/producer_comsumer.c
@@ -2,10 +2,10 @@
  * 
  * Release under GPL-3.0.
  * 
- * @file    mutex.c
+ * @file    producer_comsumer.c
  * @brief   
  * @author  gnsyxiang <gnsyxiang@163.com>
- * @date    09/04 2020 17:46
+ * @date    10/04 2020 13:40
  * @version v0.0.1
  * 
  * @since    note
@@ -13,9 +13,9 @@
  * 
  *     change log:
  *     NO.     Author              Date            Modified
- *     00      zhenquan.qiu        09/04 2020      create the file
+ *     00      zhenquan.qiu        10/04 2020      create the file
  * 
- *     last modified: 09/04 2020 17:46
+ *     last modified: 10/04 2020 13:40
  */
 #include "hal_log.h"
 #include "hal_thread.h"
@@ -23,15 +23,21 @@
 #include "hal_mem.h"
 #include "hal_assert.h"
 #include "hal_mutex.h"
+#include "hal_sem.h"
 
 static ThreadMutexHandle_t mutex_handle;
+static ThreadSemHandle_t sem_handle;
+static hal_int32_t cnt = 0;
 
 static void _producer_loop(void *args)
 {
     while (1) {
         HalMutexLock(mutex_handle);
-        HalLogW("producer loop \n");
+        cnt++;
         HalMutexUnLock(mutex_handle);
+
+        HalSemPost(sem_handle);
+        HalLogW("producer cnt: %d \n", cnt);
         Hal_sleep(1);
     }
 }
@@ -39,10 +45,13 @@ static void _producer_loop(void *args)
 static void _consumer_loop(void *args)
 {
     while (1) {
+        HalSemWait(sem_handle);
+        HalLogW("-1-consumer cnt: %d \n", cnt);
+
         HalMutexLock(mutex_handle);
-        HalLogE("consumer loop \n");
+        cnt--;
         HalMutexUnLock(mutex_handle);
-        Hal_sleep(1);
+        HalLogW("-2-consumer cnt: %d \n", cnt);
     }
 }
 
@@ -88,26 +97,29 @@ hal_int32_t main(hal_int32_t argc, const hal_char_t *argv[])
         goto L_MAIN_1;
     }
 
+    sem_handle = HalSemInit(0, 0);
+    if (NULL == sem_handle) {
+        HalLogE("init sem failed \n");
+        goto L_MAIN_2;
+    }
+
     ThreadHandle_t producer_handle = NULL;
     ThreadHandle_t consumer_handle = NULL;
 
     producer_handle = _create_thread("producer", _producer_loop, NULL);
     if (NULL == producer_handle) {
         HalLogE("create thread failed \n");
-        goto L_MAIN_2;
+        goto L_MAIN_3;
     }
 
     consumer_handle = _create_thread("consumer", _consumer_loop, NULL);
     if (NULL == consumer_handle) {
         HalLogE("create thread failed \n");
-        goto L_MAIN_3;
+        goto L_MAIN_4;
     }
 
-    int cnt = 5;
+    int cnt = 10;
     while (cnt-- > 0) {
-        HalMutexLock(mutex_handle);
-        HalLogT("main loop \n");
-        HalMutexUnLock(mutex_handle);
         Hal_sleep(1);
     }
 
@@ -115,12 +127,15 @@ hal_int32_t main(hal_int32_t argc, const hal_char_t *argv[])
     _destroy_thread(consumer_handle);
 
     HalMutexDestroy(mutex_handle);
+    HalSemDestroy(sem_handle);
 
     HalLogFinal();
 
     return 0;
-L_MAIN_3:
+L_MAIN_4:
     _destroy_thread(producer_handle);
+L_MAIN_3:
+    HalSemDestroy(sem_handle);
 L_MAIN_2:
     HalMutexDestroy(mutex_handle);
 L_MAIN_1:
