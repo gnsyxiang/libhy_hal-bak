9e173ab9b51b506b201492a7681f93ca1deac324
zhenquan.qiu <qiuzhenquan@unisound.com>
2020-04-10 15:09:09 +0800

    test: safe exit and add cond test

--- example/producer_comsumer.c
+++ example/producer_comsumer.c
@@ -24,35 +24,44 @@
 #include "hal_assert.h"
 #include "hal_mutex.h"
 #include "hal_sem.h"
+#include "hal_cond.h"
 
 static ThreadMutexHandle_t mutex_handle;
 static ThreadSemHandle_t sem_handle;
+static ThreadCondHandle_t producer_exit_cond_handle;
+static ThreadCondHandle_t consumer_exit_cond_handle;
+
 static hal_int32_t cnt = 0;
+static hal_int32_t is_running = 0;
 
 static void _producer_loop(void *args)
 {
-    while (1) {
+    while (0 == is_running) {
         HalMutexLock(mutex_handle);
         cnt++;
+        HalLogW("producer cnt: %d \n", cnt);
         HalMutexUnLock(mutex_handle);
 
         HalSemPost(sem_handle);
-        HalLogW("producer cnt: %d \n", cnt);
         Hal_sleep(1);
     }
+
+    HalCondSignal(producer_exit_cond_handle);
 }
 
 static void _consumer_loop(void *args)
 {
-    while (1) {
+    while (0 == is_running) {
         HalSemWait(sem_handle);
-        HalLogW("-1-consumer cnt: %d \n", cnt);
 
         HalMutexLock(mutex_handle);
+        HalLogW("-1-consumer cnt: %d \n", cnt);
         cnt--;
-        HalMutexUnLock(mutex_handle);
         HalLogW("-2-consumer cnt: %d \n", cnt);
+        HalMutexUnLock(mutex_handle);
     }
+
+    HalCondSignal(consumer_exit_cond_handle);
 }
 
 static ThreadHandle_t _create_thread(hal_char_t *name, HalThreadLoop_t loop, void *args)
@@ -91,55 +100,46 @@ hal_int32_t main(hal_int32_t argc, const hal_char_t *argv[])
 
     HalLogInit(&log_config);
 
-    mutex_handle = HalMutexInit();
-    if (NULL == mutex_handle) {
-        HalLogE("init mutex failed \n");
-        goto L_MAIN_1;
-    }
-
     sem_handle = HalSemInit(0, 0);
-    if (NULL == sem_handle) {
-        HalLogE("init sem failed \n");
-        goto L_MAIN_2;
-    }
+
+    mutex_handle = HalMutexInit();
+    producer_exit_cond_handle = HalCondInit();
+    consumer_exit_cond_handle = HalCondInit();
 
     ThreadHandle_t producer_handle = NULL;
     ThreadHandle_t consumer_handle = NULL;
 
     producer_handle = _create_thread("producer", _producer_loop, NULL);
-    if (NULL == producer_handle) {
-        HalLogE("create thread failed \n");
-        goto L_MAIN_3;
-    }
-
     consumer_handle = _create_thread("consumer", _consumer_loop, NULL);
-    if (NULL == consumer_handle) {
-        HalLogE("create thread failed \n");
-        goto L_MAIN_4;
-    }
 
-    int cnt = 10;
+    int cnt = 3;
     while (cnt-- > 0) {
         Hal_sleep(1);
     }
+    is_running = 1;
+    hal_int32_t ret = 0;
+
+    HalMutexLock(mutex_handle);
+    ret = HalCondWait(producer_exit_cond_handle, mutex_handle, 3000);
+    HalMutexUnLock(mutex_handle);
+
+    HalSemPost(sem_handle);
+
+    HalMutexLock(mutex_handle);
+    ret = HalCondWait(consumer_exit_cond_handle, mutex_handle, 3000);
+    HalMutexUnLock(mutex_handle);
+
+    HalMutexDestroy(mutex_handle);
+    HalCondDestroy(producer_exit_cond_handle);
+    HalCondDestroy(consumer_exit_cond_handle);
 
     _destroy_thread(producer_handle);
     _destroy_thread(consumer_handle);
 
-    HalMutexDestroy(mutex_handle);
     HalSemDestroy(sem_handle);
 
     HalLogFinal();
 
     return 0;
-L_MAIN_4:
-    _destroy_thread(producer_handle);
-L_MAIN_3:
-    HalSemDestroy(sem_handle);
-L_MAIN_2:
-    HalMutexDestroy(mutex_handle);
-L_MAIN_1:
-    HalLogFinal();
-    return -1;
 }
 
