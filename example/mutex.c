051ded79912a0fd467a75c25bef81706e72f2cf2
zhenquan.qiu <qiuzhenquan@unisound.com>
2020-04-10 11:31:32 +0800

    test: update mute test code

--- example/mutex.c
+++ example/mutex.c
@@ -17,14 +17,114 @@
  * 
  *     last modified: 09/04 2020 17:46
  */
+#include "hal_log.h"
+#include "hal_thread.h"
+#include "hal_time.h"
+#include "hal_mem.h"
+#include "hal_assert.h"
 #include "hal_mutex.h"
+
+static ThreadMutexHandle_t mutex_handle;
+
+static void _producer_loop(void *args)
+{
+    while (1) {
+        HalMutexLock(mutex_handle);
+        HalLogW("producer loop \n");
+        HalMutexUnLock(mutex_handle);
+        Hal_sleep(1);
+    }
+}
+
+static void _consumer_loop(void *args)
+{
+    while (1) {
+        HalMutexLock(mutex_handle);
+        HalLogE("consumer loop \n");
+        HalMutexUnLock(mutex_handle);
+        Hal_sleep(1);
+    }
+}
+
+static ThreadHandle_t _create_thread(hal_char_t *name, HalThreadLoop_t loop, void *args)
+{
+    Hal_assert(NULL != name);
+    Hal_assert(NULL != loop);
+
+    HalThreadLoopConfig_t loop_config;
+    Hal_memset(&loop_config, '\0', HAL_THREAD_LOOP_CONFIG_LEN);
+    loop_config.loop = loop;
+    loop_config.args = args;
+
+    HalThreadConfig_t config;
+    Hal_memset(&config, '\0', HAL_THREAD_CONFIG_LEN);
+    config.name         = name;
+    config.stack_size   = STACK_SIZE;
+    config.priority     = HAL_THREAD_PRIORITY_NORMAL;
+    config.loop_config  = &loop_config;
+
+    return HalThreadCreate(&config);
+}
+
+static inline void _destroy_thread(ThreadHandle_t handle)
+{
+    Hal_assert(NULL != handle);
+
+    HalThreadDestroy(handle);
+}
 
 hal_int32_t main(hal_int32_t argc, const hal_char_t *argv[])
 {
+    LogConfig_t log_config;
+    log_config.level        = LOG_LEVEL_VERBOSE;
+    log_config.color_flag   = LOG_COLOR_ON;
+    log_config.buf_len      = 1024;
 
-    ThreadMutexHandle_t handle = HalMutexInit();
+    HalLogInit(&log_config);
 
-    HalMutexDestroy(handle);
+    mutex_handle = HalMutexInit();
+    if (NULL == mutex_handle) {
+        HalLogE("init mutex failed \n");
+        goto L_MAIN_1;
+    }
+
+    ThreadHandle_t producer_handle = NULL;
+    ThreadHandle_t consumer_handle = NULL;
+
+    producer_handle = _create_thread("producer", _producer_loop, NULL);
+    if (NULL == producer_handle) {
+        HalLogE("create thread failed \n");
+        goto L_MAIN_2;
+    }
+
+    consumer_handle = _create_thread("consumer", _consumer_loop, NULL);
+    if (NULL == consumer_handle) {
+        HalLogE("create thread failed \n");
+        goto L_MAIN_3;
+    }
+
+    int cnt = 5;
+    while (cnt-- > 0) {
+        HalMutexLock(mutex_handle);
+        HalLogT("main loop \n");
+        HalMutexUnLock(mutex_handle);
+        Hal_sleep(1);
+    }
+
+    _destroy_thread(producer_handle);
+    _destroy_thread(consumer_handle);
+
+    HalMutexDestroy(mutex_handle);
+
+    HalLogFinal();
 
     return 0;
+L_MAIN_3:
+    _destroy_thread(producer_handle);
+L_MAIN_2:
+    HalMutexDestroy(mutex_handle);
+L_MAIN_1:
+    HalLogFinal();
+    return -1;
 }
+
