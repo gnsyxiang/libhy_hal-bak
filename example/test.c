b07df39984807113519c17d008d0883eb2c98421
zhenquan.qiu <gnsyxiang@163.com>
2021-05-07 14:30:51 +0800

    build: refactor autotools building

---
+++ example/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "module_a.h"
+
+int main(int argc, char *argv[])
+{
+    HyHelloWorld();
+
+    return 0;
+}
---
+++ src/module_a.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void HyHelloWorld(void)
+{
+    printf("hello world");
+}
