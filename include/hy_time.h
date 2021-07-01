44c43447ba6dabecfa47f5b294102d4149fa065b
zhenquan.qiu <gnsyxiang@163.com>
2021-07-01 20:16:22 +0800

    feat: add hy_error.h file

--- include/hy_time.h
+++ include/hy_time.h
@@ -28,6 +28,14 @@ extern "C" {
 
 #include "hy_type.h"
 
+
+/**
+ * @brief 在特定tick下，1s要多少个tick表示
+ *
+ * note: 默认1(tick) = 1(ms)，如果tick不是1ms，则需要修改tick个数
+ */
+#define HY_TIME_1S_REF_TICK (1000)
+
 time_t HyTimeGetTick(void);
 
 time_t HyTimeGetCurUs(void);
