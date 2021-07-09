be229123ef4b82d8cfc143b194aaab156d4ec777
zhenquan.qiu <gnsyxiang@163.com>
2021-07-09 19:18:19 +0800

    fix: fix compile bug

---
+++ src/mcu/socket_wrapper.c
@@ -0,0 +1,41 @@
+/**
+ * 
+ * Release under GPLv-3.0.
+ * 
+ * @file    socket_wrapper.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    09/07 2021 16:30
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        09/07 2021      create the file
+ * 
+ *     last modified: 09/07 2021 16:30
+ */
+#include <stdio.h>
+
+#include "socket_wrapper.h"
+
+void *socket_create(HySocketConfig_t *socket_config)
+{
+    return NULL;
+}
+
+void socket_destroy(socket_context_t *context)
+{
+
+}
+
+int socket_process(socket_context_t *context)
+{
+    return 0;
+}
+int socket_write(socket_context_t *context, void *buf, size_t len)
+{
+    return 0;
+}
---
+++ src/mcu/time_wrapper.c
@@ -0,0 +1,47 @@
+/**
+ * 
+ * Release under GPLv-3.0.
+ * 
+ * @file    time_wrapper.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    09/07 2021 16:48
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        09/07 2021      create the file
+ * 
+ *     last modified: 09/07 2021 16:48
+ */
+#include <stdio.h>
+
+#include "time_wrapper.h"
+
+time_t time_get_tick(void)
+{
+    return 0;
+}
+
+time_t time_get_cur_us(void)
+{
+    return 0;
+}
+
+time_t time_get_cur_ms(void)
+{
+    return 0;
+}
+
+void time_delay_ms(hy_uint32_t ms)
+{
+}
+
+void time_delay_us(hy_uint32_t us)
+{
+
+}
+
