43f7bad0ece73f041bfc600c7b62113675ec230c
zhenquan.qiu <gnsyxiang@163.com>
2021-08-19 20:15:26 +0800

    feat: add hy_system.c/hy_time.c/hy_uart.c files

---
+++ src/mcu/at32f4xx/hy_time.c
@@ -0,0 +1,209 @@
+/**
+ * 
+ * Release under GPLv-3.0.
+ * 
+ * @file    hy_time.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    19/08 2021 19:48
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        19/08 2021      create the file
+ * 
+ *     last modified: 19/08 2021 19:48
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "hy_time.h"
+
+#include "at32f4xx_rcc.h"
+#include "at32f4xx_tim.h"
+
+#include "hy_utils/hy_mem.h"
+#include "hy_utils/hy_string.h"
+#include "hy_utils/hy_log.h"
+#include "hy_utils/hy_assert.h"
+
+#define ALONE_DEBUG 1
+
+#if 0
+typedef struct {
+    HyTimeNum_t num;
+
+    HyTimeConfigSave_t config_save;
+} _time_context_t;
+
+static _time_context_t *context_array[HY_TIME_NUM_MAX] = {NULL};
+
+static void _init_time_rcc(HyTimeNum_t num)
+{
+    if (num == HY_TIME_NUM_2) {
+        RCC_APB1PeriphClockCmd(RCC_APB1PERIPH_TMR2, ENABLE);
+    }
+}
+
+static void _init_time_func(HyTimeNum_t num, uint16_t us, int flag)
+{
+    TMR_Type* time[] = {
+        NULL, TMR1, TMR2, TMR3, TMR4, TMR5, TMR6, TMR7, TMR8
+    };
+    TMR_TimeBaseInitType  config;
+    TMR_TimeBaseStructInit(&config);
+
+#ifdef SYSCLK_FREQ_200MHz
+    config.TMR_DIV           = 200 - 1;      // 1us
+#endif
+#ifdef SYSCLK_FREQ_240MHz
+    config.TMR_DIV           = 240 - 1;      // 1us
+#endif
+    // config.TMR_DIV           = 2400 - 1;     // 10us
+    // config.TMR_DIV           = 24000 - 1;    // 100us
+    config.TMR_Period        = us;
+    config.TMR_ClockDivision = TMR_CKD_DIV1;
+    config.TMR_CounterMode   = TMR_CounterDIR_Up;
+    config.TMR_RepetitionCounter = 0;
+
+    TMR_TimeBaseInit(time[num], &config);
+
+    TMR_Cmd(time[num], (FunctionalState)flag);
+}
+
+static void _init_time_interrupt(HyTimeNum_t num, int flag)
+{
+    if (num == HY_TIME_NUM_2) {
+        NVIC_InitType config;
+
+        config.NVIC_IRQChannel                      = TMR2_GLOBAL_IRQn;
+        config.NVIC_IRQChannelPreemptionPriority    = 0;
+        config.NVIC_IRQChannelSubPriority           = 0;
+        config.NVIC_IRQChannelCmd                   = (FunctionalState)flag;
+        NVIC_Init(&config);
+
+        TMR_INTConfig(TMR2, TMR_INT_Overflow, ENABLE);
+    }
+}
+
+static void _time_irq_handler(HyTimeNum_t num)
+{
+    if (context_array[num]) {
+        TMR_Type* time[] = {
+            NULL, TMR1, TMR2, TMR3, TMR4, TMR5, TMR6, TMR7, TMR8
+        };
+        HyTimeConfigSave_t *time_config = &context_array[num]->config_save;
+
+        if (TMR_GetINTStatus(time[num], TMR_INT_Overflow) == SET)  {
+            TMR_ClearITPendingBit(time[num], TMR_INT_Overflow);
+            if (time_config->time_cb) {
+                time_config->time_cb(time_config->args);
+            }
+        }
+    }
+}
+
+void TMR2_GLOBAL_IRQHandler(void)
+{
+    _time_irq_handler(HY_TIME_NUM_2);
+}
+
+static void _time_control_com(void *handle, int flag)
+{
+    _time_context_t *context = handle;
+    TMR_Type* time[] = {
+        NULL, TMR1, TMR2, TMR3, TMR4, TMR5, TMR6, TMR7, TMR8
+    };
+
+    if (flag) {
+        TMR_Cmd(time[context->num], ENABLE);
+    } else {
+        TMR_Cmd(time[context->num], DISABLE);
+    }
+}
+
+void HyTimeEnable(void *handle)
+{
+    HY_ASSERT_NULL_RET(!handle);
+
+    _time_control_com(handle, 1);
+}
+
+void HyTimeDisable(void *handle)
+{
+    HY_ASSERT_NULL_RET(!handle);
+
+    _time_control_com(handle, 0);
+}
+
+void HyTimeDestroy(void **handle)
+{
+    if (handle && *handle) {
+        _time_context_t *context = *handle;
+
+        context_array[context->num] = NULL;
+
+        HY_FREE(handle);
+    }
+}
+
+void *HyTimeCreate(HyTimeConfig_t *time_config)
+{
+    HY_ASSERT_NULL_RET_VAL(!time_config, NULL);
+
+    _time_context_t *context = NULL;
+
+    do {
+        context = HY_MALLOC_BREAK(sizeof(*context));
+
+        context->num                        = time_config->num;
+        context_array[time_config->num]    = context;
+
+        HY_MEMCPY(&context->config_save, &time_config->config_save);
+
+        _init_time_rcc(time_config->num);
+        _init_time_func(time_config->num, time_config->us, time_config->flag);
+        _init_time_interrupt(time_config->num, time_config->flag);
+
+        return context;
+    } while (0);
+
+    HyTimeDestroy((void **)&context);
+
+    return NULL;
+}
+#else
+void *HyTimeCreate(HyTimeConfig_t *time_config)
+{
+    return NULL;
+}
+
+void HyTimeDestroy(void **handle)
+{
+
+}
+
+void HyTimeEnable(void *handle)
+{
+
+}
+
+void HyTimeDisable(void *handle)
+{
+
+}
+
+void HyTimeDelayMs(size_t ms)
+{
+
+}
+
+void HyTimeDelayUs(size_t us)
+{
+
+}
+#endif
---
+++ src/mcu/at32f4xx/hy_uart.c
@@ -0,0 +1,304 @@
+/**
+ * 
+ * Release under GPLv-3.0.
+ * 
+ * @file    hy_uart.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    19/08 2021 19:47
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        19/08 2021      create the file
+ * 
+ *     last modified: 19/08 2021 19:47
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "hy_uart.h"
+
+#include "at32f4xx_gpio.h"
+#include "at32f4xx_usart.h"
+
+#include "hy_utils/hy_log.h"
+#include "hy_utils/hy_mem.h"
+#include "hy_utils/hy_string.h"
+#include "hy_utils/hy_assert.h"
+
+
+#define ALONE_DEBUG 1
+
+typedef struct {
+    HyUartNum_t num;
+
+    HyUartConfigSave_t config_save;
+} _uart_context_t;
+
+static _uart_context_t *context_array[HY_UART_NUM_MAX] = {NULL};
+
+typedef struct {
+    HyUartNum_t     uart;
+    GPIO_Type*      gpiox;
+    uint16_t        tx_pin;
+    uint16_t        rx_pin;
+} _uart_pin_t;
+
+typedef struct {
+    uint32_t        uart_rcc;
+    uint32_t        gpio_rcc;
+    uint32_t        gpio_remap;
+    FunctionalState new_state;
+} _uart_rcc_t;
+
+static void _init_uart_gpio(HyUartNum_t num)
+{
+    _uart_pin_t uart_pin[] = {
+        {HY_UART_NUM_1, GPIOA, GPIO_Pins_9,  GPIO_Pins_10},
+        {HY_UART_NUM_4, GPIOC, GPIO_Pins_10, GPIO_Pins_11},
+        {HY_UART_NUM_5, GPIOB, GPIO_Pins_9,  GPIO_Pins_8},
+    };
+    _uart_rcc_t uart_rcc[] = {
+        {RCC_APB2PERIPH_USART1, RCC_APB2PERIPH_GPIOA,                        0, ENABLE},
+        {RCC_APB1PERIPH_UART4,  RCC_APB2PERIPH_GPIOC,                        0, ENABLE},
+        {RCC_APB1PERIPH_UART5,  RCC_APB2PERIPH_GPIOB | RCC_APB2PERIPH_AFIO,  AFIO_MAP5_USART5_0001, ENABLE},
+    };
+
+    for (int i = 0; i < HY_UART_NUM_MAX; i++) {
+        if (num != uart_pin[i].uart) {
+            continue;
+        }
+
+        RCC_APB2PeriphClockCmd(uart_rcc[i].gpio_rcc, ENABLE);
+        if (HY_UART_NUM_1 == num) {
+            RCC_APB2PeriphClockCmd(uart_rcc[i].uart_rcc, ENABLE);
+        } else {
+            RCC_APB1PeriphClockCmd(uart_rcc[i].uart_rcc, ENABLE);
+        }
+        if (uart_rcc[i].gpio_remap) {
+            GPIO_PinsRemapConfig(uart_rcc[i].gpio_remap, uart_rcc[i].new_state);
+        }
+
+        GPIO_InitType gpio;
+
+        GPIO_StructInit(&gpio);
+        gpio.GPIO_Pins      = uart_pin[i].tx_pin; 
+        gpio.GPIO_MaxSpeed  = GPIO_MaxSpeed_50MHz;
+        gpio.GPIO_Mode      = GPIO_Mode_AF_PP;
+
+        GPIO_Init(uart_pin[i].gpiox, &gpio);
+
+        gpio.GPIO_Pins      = uart_pin[i].rx_pin;
+        gpio.GPIO_Mode      = GPIO_Mode_IN_PD;
+
+        GPIO_Init(uart_pin[i].gpiox, &gpio);
+
+        break;
+    }
+}
+
+static void _init_uart_func(HyUartNum_t num, uint32_t rate)
+{
+    USART_InitType uart;
+    USART_Type* uart_num[HY_UART_NUM_MAX] = {
+        NULL, USART1, USART2, USART3, UART4, UART5
+    };
+
+    USART_StructInit(&uart);
+    uart.USART_BaudRate              = rate;
+    uart.USART_WordLength            = USART_WordLength_8b;
+    uart.USART_StopBits              = USART_StopBits_1;
+    uart.USART_Parity                = USART_Parity_No;
+    uart.USART_HardwareFlowControl   = USART_HardwareFlowControl_None;
+    uart.USART_Mode                  = USART_Mode_Rx | USART_Mode_Tx;	
+
+    USART_Init(uart_num[num], &uart); 
+}
+
+typedef struct {
+    USART_Type  *uart;
+    uint8_t     ch;
+    uint8_t     sub_priority;
+} _uart_interrupt_t;
+
+static void _init_uart_interrupt(HyUartNum_t num)
+{
+    _uart_interrupt_t uart_interrupt[] = {
+        {NULL,      0,              0},
+        {USART1,    USART1_IRQn,    1},
+        {USART2,    USART1_IRQn,    2},
+        {USART3,    USART1_IRQn,    3},
+        {UART4,     UART4_IRQn,     4},
+        {UART5,     UART5_IRQn,     5},
+    };
+
+    NVIC_InitType nvic;
+    nvic.NVIC_IRQChannel                      = uart_interrupt[num].ch;
+    nvic.NVIC_IRQChannelPreemptionPriority    = 0;
+    nvic.NVIC_IRQChannelSubPriority           = uart_interrupt[num].sub_priority;
+    nvic.NVIC_IRQChannelCmd                   = ENABLE;
+
+    NVIC_Init(&nvic);
+
+    USART_INTConfig(uart_interrupt[num].uart, USART_INT_RDNE, ENABLE);
+    USART_Cmd(uart_interrupt[num].uart, ENABLE);
+}
+
+static void _uart_irq_handler(HyUartNum_t num)
+{
+    if (context_array[num]) {
+        HyUartConfigSave_t *config_save = &context_array[num]->config_save;
+        USART_Type *uart[HY_UART_NUM_MAX] = {
+            NULL, USART1, USART2, USART3, UART4, UART5
+        };
+
+        if(USART_GetITStatus(uart[num], USART_INT_RDNE) != RESET) {
+            if (config_save->read_cb) {
+                hy_u8_t ch = USART_ReceiveData(uart[num]);
+                config_save->read_cb(&ch, 1, config_save->args);
+            }
+        }
+    }
+}
+
+void USART1_IRQHandler(void)
+{
+    _uart_irq_handler(HY_UART_NUM_1);
+}
+
+void UART4_IRQHandler(void)
+{
+    _uart_irq_handler(HY_UART_NUM_4);
+}
+
+void UART5_IRQHandler(void)
+{
+    _uart_irq_handler(HY_UART_NUM_5);
+}
+
+int HyUartSendByte(void *handle, char byte)
+{
+    HY_ASSERT_NULL_RET_VAL(!handle, -1);
+
+    _uart_context_t *context = handle;
+    USART_Type* uart[HY_UART_NUM_MAX] = {
+        NULL, USART1, USART2, USART3, UART4, UART5
+    };
+    FlagStatus Status = RESET;
+    uint32_t Count=0;
+
+    USART_SendData(uart[context->num], byte);
+    do {
+        Status = USART_GetFlagStatus(uart[context->num], USART_FLAG_TRAC);
+        Count++;
+    } while((Status == RESET) && (Count != 0x1CE2));
+
+    if(Count == 0x1CE2)
+        return (EOF);
+
+    return (byte);
+}
+
+int HyUartSendBuf(void *handle, void *buf, size_t len)
+{
+    HY_ASSERT_NULL_RET_VAL(!handle, -1);
+
+    _uart_context_t *context = handle;
+    USART_Type* uart[HY_UART_NUM_MAX] = {
+        NULL, USART1, USART2, USART3, UART4, UART5
+    };
+    FlagStatus Status = RESET;
+    uint32_t Count=0;
+    uint16_t i = 0;
+    char *str = buf;
+
+    do {
+        Count = 0;
+        USART_SendData(uart[context->num], str[i]);
+        do {
+            Status = USART_GetFlagStatus(uart[context->num], USART_FLAG_TRAC);
+            Count++;
+        } while((Status == RESET) && (Count != 0x1CE2));
+
+        if(Count == 0x1CE2) {
+            break;
+        }
+        i++;
+    } while (i < len);
+
+    return (i);
+}
+
+void HyUartDestroy(void **handle)
+{
+    LOGT("%s:%d \n", __func__, __LINE__);
+
+    if (handle && *handle) {
+        _uart_context_t *context = *handle;
+
+        context_array[context->num] = NULL;
+
+        HY_FREE(handle);
+    }
+}
+
+void *HyUartCreate(HyUartConfig_t *uart_config)
+{
+    LOGT("%s:%d \n", __func__, __LINE__);
+
+    HY_ASSERT_NULL_RET_VAL(!uart_config, NULL);
+
+    _uart_context_t *context = NULL;
+
+    do {
+        context = HY_MALLOC_BREAK(sizeof(*context));
+
+        context_array[uart_config->num] = context;
+        context->num                    = uart_config->num;
+
+        HY_MEMCPY(&context->config_save, &uart_config->config_save);
+
+        _init_uart_gpio(uart_config->num);
+        _init_uart_func(uart_config->num, uart_config->rate);
+        _init_uart_interrupt(uart_config->num);
+
+        return context;
+    } while (0);
+
+    HyUartDestroy((void **)&context);
+
+    return NULL;
+}
+
+#ifdef DEBUG_UART
+
+int _write(int fd, char *ptr, int len)
+{
+    /*
+     * write "len" of char from "ptr" to file id "fd"
+     * Return number of char written.
+     *
+     * Only work for STDOUT, STDIN, and STDERR
+     */
+    if (fd > 2) {
+        return -1;
+    }
+
+    int i = 0;
+    while (*ptr && (i < len)) {
+        if (*ptr == '\n') {
+            HyUartSendByte(context_array[DEBUG_UART_NUM], '\r');
+        }
+        HyUartSendByte(context_array[DEBUG_UART_NUM], *ptr);
+        i++;
+        ptr++;
+    }
+    return i;
+}
+#endif
+
