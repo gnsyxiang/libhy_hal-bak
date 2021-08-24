3c87c2de4fa86ca4ed1e028af3644963d95ca9da
zhenquan.qiu <gnsyxiang@163.com>
2021-08-24 10:31:06 +0800

    refactor: refactor for linux

--- example/hy_uart_demo.c
+++ example/hy_uart_demo.c
@@ -70,8 +70,8 @@ static _main_context_t *_module_create(void)
 
     HyUartConfig_t uart_config;
     uart_config.dev_name                = "/dev/ttyUSB0";
-    uart_config.rate                    = HY_RATE_115200;
-    uart_config.flow_control            = HY_UART_FLOW_CONTROL_DISABLE;
+    uart_config.rate                    = HY_UART_RATE_115200;
+    uart_config.flow_control            = HY_UART_FLOW_CONTROL_NONE;
     uart_config.bits                    = HY_UART_BITS_8;
     uart_config.parity                  = HY_UART_PARITY_N;
     uart_config.stop                    = HY_UART_STOP_1;
